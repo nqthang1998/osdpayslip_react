2515be280a955b08b17976c2cb4e0b17a06a0f09
Thang Nguyen Quoc <thang.nguyen@orientsoftware.com>
2019-08-28 14:20:21 +0700

    up to load

---
+++ OSDPayslip.Web/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSDPayslip.Web
+{
+    public class Class
+    {
+        private double dasdasd { get; set; }
+    }
+}
